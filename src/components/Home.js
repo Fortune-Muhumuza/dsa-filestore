150c0db78152e5711060a42b708c8ac4e8ea06bb
Fortune <fortunemuhumuza@gmail.com>
2021-07-18 15:51:02 -0700

    url for download file generated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './Home.css'
+import Header from './Header'
 
 const Home = () => {
     return (
         <div className="home">
-            Home
+            <Header />
+            <h1>Home page</h1>
+            <div className='home__container'>
+
+
+                <div className='home__headerCard'>
+                    <h2>Peterson Mugume - Year 1</h2>
+                </div>
+                <h2>Course units</h2>
+                <table className='home__table'>
+        <tr>
+                <th>Name</th>
+                <th>Tutor</th>
+                <th>Contact</th>
+              </tr>
+              <tr>
+                <td>Microbiologu</td>
+                <td>Dr. Bwambale Phineas</td>
+                <td>0759391123</td>
+              </tr>
+              </table>
+            </div>
         </div>
     )
 }
